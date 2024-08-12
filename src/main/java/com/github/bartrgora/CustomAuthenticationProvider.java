7c120602afc7f8d04123f5d3664852665c2bcd15
Góra Bartłomiej <Bartlomiej.Gora@cyfra.gov.pl>
2024-08-12 14:25:34 +0200

    Added CustomAuthenticationProvider

---
+++ src/main/java/com/github/bartrgora/CustomAuthenticationProvider.java
@@ -0,0 +1,38 @@
+package com.github.bartrgora;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public CustomAuthenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        var username = authentication.getName();
+        var password = authentication.getCredentials().toString();
+        var userDetails = userDetailsService.loadUserByUsername(username);
+        if (passwordEncoder.matches(password, userDetails.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+        } else {
+            throw new BadCredentialsException("Bad credentials");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
--- src/main/java/com/github/bartrgora/WebAuthorizationConfig.java
+++ src/main/java/com/github/bartrgora/WebAuthorizationConfig.java
@@ -2,6 +2,7 @@ package com.github.bartrgora;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -9,10 +10,16 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class WebAuthorizationConfig {
 
+    private final AuthenticationProvider customAuthenticationProvider;
+
+    public WebAuthorizationConfig(AuthenticationProvider customAuthenticationProvider) {
+        this.customAuthenticationProvider = customAuthenticationProvider;
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.httpBasic(Customizer.withDefaults());
+        http.authenticationProvider(this.customAuthenticationProvider);
         http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
 
         return http.build();
